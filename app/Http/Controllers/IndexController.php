c0289c2ad44c2c92f2a2d818901e3665845be6ac
Evan <zhh1208@sina.cn>
2020-04-26 12:13:51 +0800

    企业微信授权优化

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -24,13 +24,13 @@ class IndexController extends Controller
         $wework = Factory::work(Config::get('wework'));
 
         if ($request->has('code')) {
-            $user = $wework->oauth->user();
+            $user = $wework->oauth->user()->getOriginal();
 
             /**
              * 当用户为企业成员时返回 UserId，否则返回 OpenId
              * @see https://work.weixin.qq.com/api/doc#90000/90135/91023
              */
-            if (!isset($user->userId)) {
+            if (!isset($user['UserId'])) {
                 //TODO 待补全错误页面地址
                 return redirect('非企业成员跳至错误页面');
             }
@@ -39,13 +39,13 @@ class IndexController extends Controller
              * 根据openid获取手机号，查看当前登陆的人是否在白名单中
              */
 
-            $userInfo = DB::select('SELECT TOP 1 * FROM user WHERE userId = ?', [$user->userId]);
+            $userInfo = DB::select('SELECT TOP 1 * FROM user WHERE userId = ?', [$user['UserId']]);
 
             if (empty($userInfo)) {
                 /**
                  * 为空说明当前userId数据库不存在，需获取手机号跟数据库白名单指定用户(userId相同)做绑定
                  */
-                $data = $wework->user->get($user->userId);
+                $data = $wework->user->get($user['UserId']);
                 $userInfo = DB::select('SELECT TOP 1 * FROM user WHERE phone = ?', [$data['mobile']]);
                 if (empty($userInfo)) {
                     //TODO 待补全错误页面地址
@@ -55,7 +55,7 @@ class IndexController extends Controller
                 /**
                  * 企业微信与白名单绑定操作
                  */
-                DB::update('UPDATE user SET userId = ?,join_time = ?,status = ? WHERE phone = ?', [$user->userId, Carbon::now(), 1, $data['mobile']]);
+                DB::update('UPDATE user SET userId = ?,join_time = ?,status = ? WHERE phone = ?', [$user['UserId'], Carbon::now(), 1, $data['mobile']]);
 
             }
 
